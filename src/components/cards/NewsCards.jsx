cfeffc0d60411a32063bae414975b04d527c2100
itkncoder <kncoderr@gmail.com>
2024-03-24 15:00:10 +0500

    change

--- src/components/cards/NewsCards.jsx
+++ src/components/cards/NewsCards.jsx
@@ -17,13 +17,13 @@ function NewsCards() {
             {
                 news?.docs.map(i => (
                     <div
-                        className='flex justify-center h-[480px] items-center flex-col w-[95%] md:w-[350px] border text-center pb-10 rounded-[20px]'
+                        className='flex justify-center h-[550px] items-center flex-col w-[95%] md:w-[350px] border text-center pb-10 rounded-[20px]'
                         data-aos='zoom-in'
                     >
                         <img
                             src={i.data().image}
                             alt='404'
-                            className='rounded-t-[20px] h-[250px] w-full object-cover'
+                            className='rounded-t-[20px] min-h-[250px] h-[250px] w-full object-cover'
                         />
                         <h1 className='text-[#3a3a3a] font-bold text-[24px] mt-5'>
                             {i.data().title}
