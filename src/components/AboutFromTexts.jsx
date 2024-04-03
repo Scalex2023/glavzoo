1ce3f9812c3756eceb45215851c6f939e7a76c74
Scalex2023 <164862257+Scalex2023@users.noreply.github.com>
2024-04-03 17:46:47 +0500

    Add files via upload

---
+++ src/components/AboutFromTexts.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+function AboutFromTexts() {
+	return (
+		<div className='mx-auto py-10 mt-[100px]'>
+			<div className='flex flex-col md:flex-row px-5 justify-center items-center gap-20'>
+				<div className='flex flex-col items-start gap-5'>
+					<div>
+						<h2 className='text-xl font-bold text-gray-800 mb-2'>Адрес:</h2>
+						<ul className='list-disc pl-5'>
+							<li className='text-base font-medium text-gray-700'>
+								г. Ростов-на-Дону, пер. Элеваторный 4А
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h2 className='text-xl font-bold text-gray-800 mb-2'>Телефон:</h2>
+						<ul>
+							<li className='text-base font-medium text-gray-700'>
+								<a href='tel:+78632695017'>+7 (863) 269-50-17</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<h2 className='text-xl font-bold text-gray-800 mb-2'>Email:</h2>
+						<ul className='list-disc pl-5'>
+							<li className='text-base font-medium text-gray-700'>
+								info@glavzoo.ru
+							</li>
+						</ul>
+					</div>
+					<div>
+						<div className='hidden md:block w-64 h-64 bg-gray-200 rounded-lg'>
+							<img
+								src='https://play-lh.googleusercontent.com/uqCdtOFFI_PkFV_9Vm0Ad5Wf1WPJH3rtDSG_tRZ6KVDcWMn14K6fRXffQUeaqCW04g'
+								alt='img'
+								className='object-cover w-full h-full rounded-lg'
+							/>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h2 className='text-xl font-bold text-gray-800 mb-2'>
+						Яндекс карты, точка:
+					</h2>
+					<div className='w-60 h-60 md:w-96 md:h-96 bg-gray-200 rounded-lg'>
+						<iframe
+							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2708.2414201891374!2d39.6258714!3d47.2509823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3c7643faef0a9%3A0x69d11e0b938ca7f7!2z0K3Qu9C10LLQsNGC0L7RgNC90YvQuSDQv9C10YAuLCA00JAsINCg0L7RgdGC0L7Qsi3QvdCwLdCU0L7QvdGDLCDQoNC-0YHRgtC-0LLRgdC60LDRjyDQvtCx0LsuLCDQoNC-0YHRgdC40Y8sIDM0NDAwMA!5e0!3m2!1sru!2s!4v1712145165652!5m2!1sru!2s'
+							width='100%'
+							height='100%'
+							style={{ border: '0' }}
+							allowfullscreen=''
+							loading='lazy'
+							referrerpolicy='no-referrer-when-downgrade'
+							className='rounded-lg'
+						></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default AboutFromTexts
