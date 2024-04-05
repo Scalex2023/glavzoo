a693cee3b66b3e1cb7dff425f654774e075499f7
Scalex2023 <164862257+Scalex2023@users.noreply.github.com>
2024-04-05 14:07:44 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
 					<img src={logo} alt='logo' className='opacity-80' />
 					<p className='ml-5 text-[17px] text-[#ffffff]'>© {new Date().getFullYear()}. Все права защищены.</p>
 					<p className='ml-5 text-[17px] text-[#ffffff]'>
-					<a href="https://glavzoo.ru/policy.pdf">Политика конфиденциальности</a></p>
+					<a href="https://glavzoo.ru/policy.pdf" target="_blank">Политика конфиденциальности</a></p>
 							
 					
 				</aside>
